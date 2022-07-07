937929059d30ca0c91c89b23e236d6e12cde3308
Tales <jobbtales@gmail.com>
2022-07-07 10:40:40 -0300

    feat: added component Signup to Routes

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,9 +1,10 @@
 import { Switch, Route } from "react-router-dom"
+import SignUp from "../pages/Signup"
 
 export default function Routes() {
     return (
         <Switch>
-            {/* <Route component={Home} exact path={'/'}/> */}
+            <Route component={SignUp} exact path={'/signup'}/>
         </Switch>
     )
 }
