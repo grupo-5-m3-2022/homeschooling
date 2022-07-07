9207a706efba89e4e6dad3eb1518bff943886e75
MarcosMiraco <marcosjosue572@gmail.com>
2022-07-07 12:24:26 -0300

    Fix: Merge Conflits Resolved and Login Path Added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import Routes from "./routes"
-import GlobalStyle from "./styles/GlobalStyle"
 
 export default function App() {
   return (
     <>
-    <GlobalStyle />
-    <Routes />
+      <Routes />
     </>
   )
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { GlobalStyle } from './styles/global';
+import { GlobalStyle } from './style/global';
 import { BrowserRouter } from "react-router-dom"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
