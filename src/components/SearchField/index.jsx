968289001a2bd05382db3a23766015ac9f2aae91
Marcos Josué <marcosjosue572@gmail.com>
2022-07-07 18:58:35 -0300

    Fix: Bimesters Now Are Dynamic to Material

--- src/components/SearchField/index.jsx
+++ src/components/SearchField/index.jsx
@@ -1,13 +1,13 @@
 import { AiOutlineSearch } from "react-icons/ai"
 import { SearchInputDiv, SearchSelectStyle, SearchInputStyle } from "./styles"
 
-export default function SearchInput({placeholder, options}) {
+export default function SearchInput({placeholder, options, optionSetter}) {
     return (
         <SearchInputDiv>
             <AiOutlineSearch />
             {
                 options ? 
-                    <SearchSelectStyle>
+                    <SearchSelectStyle onChange={Event => optionSetter(Event.target.value)}>
                         {
                             Array.isArray(options) && options.map(({valor, texto}, index) => (
                                 <option key={index} value={valor}>{texto}</option>
