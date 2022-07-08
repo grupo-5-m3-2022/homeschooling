5bd367dfba1440f2043c1a797a69e340bc4b4b0a
Tales <jobbtales@gmail.com>
2022-07-08 14:34:25 -0300

    feat: added toast container

--- src/index.js
+++ src/index.js
@@ -3,10 +3,23 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { GlobalStyle } from './style/global';
 import { BrowserRouter } from "react-router-dom"
+import { ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
+    <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
     <GlobalStyle />
     <App />
   </BrowserRouter>
