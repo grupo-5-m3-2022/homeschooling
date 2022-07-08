c841c0222c400dc4a40af71a54e91b4ceba106f9
Gabriel Silverio <gabrielriibeiro@gmail.com>
2022-07-08 17:14:17 +0100

    static-grade-page-created

---
+++ src/components/GradeCard/style.js
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const ContainerCard = styled.div`
+    width: 100%;
+    height: 35px;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+
+    background-color: #87CEFA;
+    border: 1px solid #87CEFA;
+    border-radius: 2px;
+    margin: 8px 0;
+
+    span{
+        color: black;
+        font-size: 14px;
+    }
+
+`
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -2,6 +2,7 @@ import { Switch, Route, Redirect } from "react-router-dom"
 import SignUp from "../pages/Signup"
 import Dashboard from "../pages/dashboard"
 import Login from "../pages/Login"
+import Grade from "../pages/Grade"
 
 export default function Routes() {
     return (
@@ -9,6 +10,7 @@ export default function Routes() {
             <Route component={Login} exact path={'/'}/>
             <Route component={SignUp} exact path={'/signup'}/>
             <Route component={Dashboard} exact path={'/dashboard'}/>
+            <Route component={Grade} exact path={'/grade'}/>
             {/* <Route component={Notfound} exact path={'/404'}/>
             <Redirect to="/404"/> */}
         </Switch>
