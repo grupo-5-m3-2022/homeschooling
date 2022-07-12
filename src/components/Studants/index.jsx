1979bbee2f2e66de916b94ac6f96769ede8dcf8e
Tales <jobbtales@gmail.com>
2022-07-12 18:37:52 -0300

    feat: add student 95% done

--- src/components/Studants/index.jsx
+++ src/components/Studants/index.jsx
@@ -4,7 +4,12 @@ import { FiUser } from "react-icons/fi"
 import { IoPersonRemoveSharp } from "react-icons/io5"
 import { AiOutlineCloseCircle } from "react-icons/ai"
 import Modal from 'react-modal';
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useUserStates } from "../Providers"
+import api from "../../services/api"
+import { useForm } from 'react-hook-form'
+import * as yup from 'yup'
+import { yupResolver } from '@hookform/resolvers/yup'
 
 const customStyles = {
     content: {
@@ -18,8 +23,44 @@ const customStyles = {
   };
 
 export default function Studants() {
-    const [modalAddStudant, setModalAddStudant] = useState(false)
+    const {user} = useUserStates()
+    const token = localStorage.getItem("@token")
+    const id = localStorage.getItem("@userId")
+    const [studantsList, setStudantsList] = useState([])
+    const length = studantsList?.students?.length || 0
 
+    async function loadStudants() {
+        const res = await api.get('/connections', {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }, 
+            params: {
+                professorEmail: user.email
+            }
+        })
+        .then(response => {
+            setStudantsList(response.data[0])
+        })
+        .catch(err => console.log(err))
+
+        return res
+    }
+
+    useEffect(() => {
+        loadStudants()
+    })
+    
+    const schema = yup.object().shape({
+        email: yup.string().required('Campo obrigatório').email('Email inválido'),
+        name: yup.string().required('Campo obrigatório').min(2, 'Mínimo de 2 caractéres')
+    })
+    const {register, handleSubmit, formState: {errors}} = useForm({resolver: yupResolver(schema)})
+
+    function onSubmit(data) {
+        console.log(data)
+    }
+
+    const [modalAddStudant, setModalAddStudant] = useState(false)
     function handleOpenModal() {
         setModalAddStudant(true)
     }
@@ -37,7 +78,7 @@ export default function Studants() {
                     </Icon>
                         <TotalContent>
                             <p>Total de alunos(as):</p>
-                            <h3>5</h3>
+                            <h3>{length}</h3>
                         </TotalContent>
                 </TotalStudants>
 
@@ -45,19 +86,21 @@ export default function Studants() {
                     <button onClick={handleOpenModal}><span>+</span> Adicionar Aluno</button>
                 </ButtonContainer>
 
-                <Modal isOpen={modalAddStudant} onRequestClose={handleCloseModal} style={customStyles}>
+                <Modal ariaHideApp={false} isOpen={modalAddStudant} onRequestClose={handleCloseModal} style={customStyles}>
                     <ModalHeader>
                         <h4>Cadastrar Aluno</h4>
                         <button onClick={handleCloseModal}><AiOutlineCloseCircle /></button>
                     </ModalHeader>
 
                     <ModalBody>
-                        <ModalContent>
-                            <input placeholder="Digite o e-mail do(a) aluno(a)"/>
-                            <input placeholder="Digite o nome do(a) aluno(a)"/>
+                        <ModalContent onSubmit={handleSubmit(onSubmit)}>
+                            {errors?.email?.message && <span>- {errors.email.message}</span>}
+                            <input {...register("email")} placeholder="Digite o e-mail do(a) aluno(a)"/>
+                            {errors?.name?.message && <span>- {errors.name.message}</span>}
+                            <input {...register("name")} placeholder="Digite o nome do(a) aluno(a)"/>
                             <div>
                                 <button onClick={handleCloseModal} className="btnClose">Cancelar</button>
-                                <button className="btnSend">Enviar</button>
+                                <button type="submit" className="btnSend">Enviar</button>
                             </div>
                         </ModalContent>
                     </ModalBody>
@@ -65,44 +108,20 @@ export default function Studants() {
             </Header>
 
             <Body>
-                <CardStudant>
+                {studantsList?.students?.map((student, index) => (
+                <CardStudant key={index}>
                     <UserName>
                         <span><FiUser /></span>
-                        <p>Aluno 1</p>
+                        <p>{student.studentName}</p>
                     </UserName>
                     <UserEmail>
-                        <p>aluno@aluno.com</p>
+                        <p>{student.studentEmail}</p>
                     </UserEmail>
                     <UserRemove>
                         <IoPersonRemoveSharp />
                     </UserRemove>
                 </CardStudant>
-
-                <CardStudant>
-                    <UserName>
-                        <span><FiUser /></span>
-                        <p>Aluno 2</p>
-                    </UserName>
-                    <UserEmail>
-                        <p>aluno@aluno.com</p>
-                    </UserEmail>
-                    <UserRemove>
-                        <IoPersonRemoveSharp />
-                    </UserRemove>
-                </CardStudant>
-
-                <CardStudant>
-                    <UserName>
-                        <span><FiUser /></span>
-                        <p>Aluno 3</p>
-                    </UserName>
-                    <UserEmail>
-                        <p>aluno@aluno.com</p>
-                    </UserEmail>
-                    <UserRemove>
-                        <IoPersonRemoveSharp />
-                    </UserRemove>
-                </CardStudant>
+                ))}
             </Body>
         </Container>
     )
