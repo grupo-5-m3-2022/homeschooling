43a49ae075fb34ffe9d722a394a45ddd2054aadb
Marcos Josué <marcosjosue572@gmail.com>
2022-07-13 19:54:52 -0300

    Feat: Add New Extra Lessons Complete

--- src/components/SearchField/index.jsx
+++ src/components/SearchField/index.jsx
@@ -1,13 +1,13 @@
 import { AiOutlineSearch } from "react-icons/ai"
 import { SearchInputDiv, SearchSelectStyle, SearchInputStyle } from "./styles"
 
-export default function SearchInput({placeholder, options, optionSetter}) {
+export default function SearchInput({placeholder, options, optionSetter, register= () => {}, type}) {
     return (
         <SearchInputDiv>
             <AiOutlineSearch />
             {
                 options ? 
-                    <SearchSelectStyle onChange={Event => optionSetter(Event.target.value)}>
+                    <SearchSelectStyle onChange={Event => optionSetter(Event.target.value)} {...register(type)}>
                         {
                             Array.isArray(options) && options.map(({valor, texto}, index) => (
                                 <option key={index} value={valor}>{texto}</option>
