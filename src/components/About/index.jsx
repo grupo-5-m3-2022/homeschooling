b013e4fc5b69de9d1f0d007c40d1a6ddab970261
Gabriel Silverio <gabrielriibeiro@gmail.com>
2022-07-14 13:37:54 +0100

    infos da plataforma

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -1,10 +1,14 @@
+import { Container } from "./style";
 
 export default function About() {
    
       
         return (
-            <>
-                Ajuda
-            </>
+            <Container>
+                <h3>Fale conosco</h3>
+
+                <span>Email:  homeschooling@home.com</span>
+                <span>Tel: +55 (55) 5555-5555</span>
+            </Container>
         )
 }
---
+++ src/components/About/style.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    width: 100%;
+
+    h2{
+        color: black;
+        font-size: 16;
+    }
+
+    span{
+        color: black;
+        font-size: 14px;
+    }
+`
